7cc3a7c675c60385c79dc75b960af8f90cc2aa34
shyamgarg <92663424+shyamgarg@users.noreply.github.com>
2022-02-13 20:49:29 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -11,10 +11,10 @@ function update()
       }
     
     //Debug the code to store list of images in updatedImage. Use images[i]
-    var updatedImage =[images = "https://i.postimg.cc/MGn9GJXw/family.jpg","https://i.postimg.cc/qqyYvVbq/grandpa.jpg", "https://i.postimg.cc/wjMnFtMX/father.jpg" , "https://i.postimg.cc/5ymDKL83/bro.jpg", "https://i.postimg.cc/JnL6wtrd/sister.jpg", "https://i.postimg.cc/bw5W5zSK/mother.jpg"];
+    var updatedImage =images[i];
    
     //Debug the code to store list of names in updatedName. Use names[i]
-    var updatedName = [names="Fmaily Book","Ranbir Singh", "Diljeet Singh", "Rocky Singh", "Alia Singh", "Soni Singh"];
+    var updatedName =  names[i];
     
     document.getElementById("family_member_image").src = updatedImage;
     document.getElementById("family_member_name").innerHTML = updatedName;
